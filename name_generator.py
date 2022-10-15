305c0122e77345e1a7c4ac7a641c09c0fcadd0fd
Vasi_lena <105110262+Copynook@users.noreply.github.com>
2022-10-15 18:22:50 +0300

    Create name_generator.py

---
+++ name_generator.py
@@ -0,0 +1,10 @@
+print("Hello! Congrats on your decision to create a band :)")
+print("\nUse this program to generate a cool name for your music band.")
+
+print("\nWhat is the name of your hometown?")
+hometown = input("My hometown is: ")
+
+print("\nHave you ever had a pet? How did you name it?")
+pet_name = input("The name of my pet was: ")
+
+print("\nAwesome! Your band's name is: ", hometown + " " + pet_name)
